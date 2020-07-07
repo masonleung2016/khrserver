96b30b3d6f4956de78127dde06beffca15aece30
masonleung2016 <masonleung@163.com>
2020-07-07 16:32:29 +0800

    Create MenuRole.java

---
+++ khr-model/src/main/java/com/ufostudio/khr/model/MenuRole.java
@@ -0,0 +1,39 @@
+package com.ufostudio.khr.model;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:47
+ * @Package: com.ufostudio.khr.model
+ */
+
+public class MenuRole {
+    private Integer id;
+
+    private Integer mid;
+
+    private Integer rid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+}
