b45e52c2387abb98906dd72db24061edcff9f069
masonleung2016 <masonleung@163.com>
2020-07-07 16:37:09 +0800

    Create MenuService.java

---
+++ khr-service/src/main/java/com/ufostudio/khr/service/MenuService.java
@@ -0,0 +1,55 @@
+package com.ufostudio.khr.service;
+
+import com.ufostudio.khr.mapper.MenuMapper;
+import com.ufostudio.khr.mapper.MenuRoleMapper;
+import com.ufostudio.khr.model.Hr;
+import com.ufostudio.khr.model.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:55
+ * @Package: com.ufostudio.khr.service
+ */
+
+@Service
+@CacheConfig(cacheNames = "menus_cache")
+public class MenuService {
+    @Autowired
+    MenuMapper menuMapper;
+    @Autowired
+    MenuRoleMapper menuRoleMapper;
+    public List<Menu> getMenusByHrId() {
+        return menuMapper.getMenusByHrId(((Hr) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+    }
+
+    @Cacheable
+    public List<Menu> getAllMenusWithRole() {
+        return menuMapper.getAllMenusWithRole();
+    }
+
+    public List<Menu> getAllMenus() {
+        return menuMapper.getAllMenus();
+    }
+
+    public List<Integer> getMidsByRid(Integer rid) {
+        return menuMapper.getMidsByRid(rid);
+    }
+
+    @Transactional
+    public boolean updateMenuRole(Integer rid, Integer[] mids) {
+        menuRoleMapper.deleteByRid(rid);
+        if (mids == null || mids.length == 0) {
+            return true;
+        }
+        Integer result = menuRoleMapper.insertRecord(rid, mids);
+        return result==mids.length;
+    }
+}
