49dd16169156cbac7a06f0da423c8af723e5fa2d
masonleung2016 <masonleung@163.com>
2020-07-07 16:49:32 +0800

    Create LoginController.java

---
+++ khr-web/src/main/java/com/ufostudio/khr/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.ufostudio.khr.controller;
+
+import com.ufostudio.khr.config.VerificationCode;
+import com.ufostudio.khr.model.RespBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 10:26
+ * @Package: com.ufostudio.khr.controller
+ */
+
+@RestController
+public class LoginController {
+    @GetMapping("/login")
+    public RespBean login() {
+        return RespBean.error("尚未登录，请登录!");
+    }
+
+    @GetMapping("/verifyCode")
+    public void verifyCode(HttpServletRequest request, HttpServletResponse resp) throws IOException {
+        VerificationCode code = new VerificationCode();
+        BufferedImage image = code.getImage();
+        String text = code.getText();
+        HttpSession session = request.getSession(true);
+        session.setAttribute("verify_code", text);
+        VerificationCode.output(image,resp.getOutputStream());
+    }
+}
