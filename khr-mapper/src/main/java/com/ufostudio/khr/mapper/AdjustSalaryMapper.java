a5eea440032cf9bfc2b340c50ea6da909573c251
masonleung2016 <masonleung@163.com>
2020-07-07 16:14:52 +0800

    Create AdjustSalaryMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/AdjustSalaryMapper.java
@@ -0,0 +1,23 @@
+package com.ufostudio.khr.mapper;
+
+import com.ufostudio.khr.model.AdjustSalary;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:24
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface AdjustSalaryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(AdjustSalary record);
+
+    int insertSelective(AdjustSalary record);
+
+    AdjustSalary selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(AdjustSalary record);
+
+    int updateByPrimaryKey(AdjustSalary record);
+}
