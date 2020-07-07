c777a358b5141db563618c5d7f6b1648432b706a
masonleung2016 <masonleung@163.com>
2020-07-07 16:17:55 +0800

    Create HrMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/HrMapper.java
@@ -0,0 +1,39 @@
+package com.ufostudio.khr.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import com.ufostudio.khr.model.Hr;
+import com.ufostudio.khr.model.Role;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:27
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface HrMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Hr record);
+
+    int insertSelective(Hr record);
+
+    Hr selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Hr record);
+
+    int updateByPrimaryKey(Hr record);
+
+    Hr loadUserByUsername(String username);
+
+    List<Role> getHrRolesById(Integer id);
+
+    List<Hr> getAllHrs(@Param("hrid") Integer hrid, @Param("keywords") String keywords);
+
+    List<Hr> getAllHrsExceptCurrentHr(Integer id);
+
+    Integer updatePasswd(@Param("hrid") Integer hrid, @Param("encodePass") String encodePass);
+
+    Integer updateUserface(@Param("url") String url, @Param("id") Integer id);
+}
