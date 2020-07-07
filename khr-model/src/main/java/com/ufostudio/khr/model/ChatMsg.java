7ee149e295bd15ba5dde698c2692988a73099407
masonleung2016 <masonleung@163.com>
2020-07-07 16:24:30 +0800

    Create ChatMsg.java

---
+++ khr-model/src/main/java/com/ufostudio/khr/model/ChatMsg.java
@@ -0,0 +1,57 @@
+package com.ufostudio.khr.model;
+
+import java.util.Date;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:38
+ * @Package: com.ufostudio.khr.model
+ */
+
+public class ChatMsg {
+    private String from;
+    private String to;
+    private String content;
+    private Date date;
+    private String fromNickname;
+
+    public String getFromNickname() {
+        return fromNickname;
+    }
+
+    public void setFromNickname(String fromNickname) {
+        this.fromNickname = fromNickname;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
