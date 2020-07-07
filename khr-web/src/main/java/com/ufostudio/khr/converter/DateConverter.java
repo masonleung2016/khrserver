e102ede328b57f73da3a543820d75a95e728fa6d
masonleung2016 <masonleung@163.com>
2020-07-07 16:52:31 +0800

    Create DateConverter.java

---
+++ khr-web/src/main/java/com/ufostudio/khr/converter/DateConverter.java
@@ -0,0 +1,29 @@
+package com.ufostudio.khr.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 10:27
+ * @Package: com.ufostudio.khr.converter
+ */
+
+@Component
+public class DateConverter implements Converter<String, Date> {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public Date convert(String source) {
+        try {
+            return sdf.parse(source);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
