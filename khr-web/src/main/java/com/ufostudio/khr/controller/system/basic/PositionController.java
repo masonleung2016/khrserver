6aad6e0ede8123f5ab7c35bd00f329dbe120734d
masonleung2016 <masonleung@163.com>
2020-07-07 16:47:09 +0800

    Create PositionController.java

---
+++ khr-web/src/main/java/com/ufostudio/khr/controller/system/basic/PositionController.java
@@ -0,0 +1,58 @@
+package com.ufostudio.khr.controller.system.basic;
+
+import com.ufostudio.khr.model.Position;
+import com.ufostudio.khr.model.RespBean;
+import com.ufostudio.khr.service.PositionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 10:23
+ * @Package: com.ufostudio.khr.controller.system.basic
+ */
+
+@RestController
+@RequestMapping("/system/basic/pos")
+public class PositionController {
+    @Autowired
+    PositionService positionService;
+    @GetMapping("/")
+    public List<Position> getAllPositions() {
+        return positionService.getAllPositions();
+    }
+
+    @PostMapping("/")
+    public RespBean addPosition(@RequestBody Position position) {
+        if (positionService.addPosition(position) == 1) {
+            return RespBean.ok("添加成功!");
+        }
+        return RespBean.error("添加失败!");
+    }
+
+    @PutMapping("/")
+    public RespBean updatePositions(@RequestBody Position position) {
+        if (positionService.updatePositions(position) == 1) {
+            return RespBean.ok("更新成功!");
+        }
+        return RespBean.error("更新失败!");
+    }
+
+    @DeleteMapping("/{id}")
+    public RespBean deletePositionById(@PathVariable Integer id) {
+        if (positionService.deletePositionById(id) == 1) {
+            return RespBean.ok("删除成功!");
+        }
+        return RespBean.error("删除失败!");
+    }
+
+    @DeleteMapping("/")
+    public RespBean deletePositionsByIds(Integer[] ids) {
+        if (positionService.deletePositionsByIds(ids) == ids.length) {
+            return RespBean.ok("删除成功!");
+        }
+        return RespBean.error("删除失败!");
+    }
+}
