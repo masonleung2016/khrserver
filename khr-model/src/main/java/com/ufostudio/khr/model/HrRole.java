6f0e60ad300d9855f37109fdccc2d9c1b9175500
masonleung2016 <masonleung@163.com>
2020-07-07 16:25:46 +0800

    Create HrRole.java

---
+++ khr-model/src/main/java/com/ufostudio/khr/model/HrRole.java
@@ -0,0 +1,39 @@
+package com.ufostudio.khr.model;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:43
+ * @Package: com.ufostudio.khr.model
+ */
+
+public class HrRole {
+    private Integer id;
+
+    private Integer hrid;
+
+    private Integer rid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getHrid() {
+        return hrid;
+    }
+
+    public void setHrid(Integer hrid) {
+        this.hrid = hrid;
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+}
