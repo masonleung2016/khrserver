8183af75d1acba0ab071f9e2a8d4bcb38bbbd0ac
masonleung2016 <masonleung@163.com>
2020-07-07 16:21:02 +0800

    Create PoliticsstatusMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/PoliticsstatusMapper.java
@@ -0,0 +1,27 @@
+package com.ufostudio.khr.mapper;
+
+import com.ufostudio.khr.model.Politicsstatus;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:30
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface PoliticsstatusMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Politicsstatus record);
+
+    int insertSelective(Politicsstatus record);
+
+    Politicsstatus selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Politicsstatus record);
+
+    int updateByPrimaryKey(Politicsstatus record);
+
+    List<Politicsstatus> getAllPoliticsstatus();
+}
