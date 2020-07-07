e531ad07e7e39de47242f06492d3efa1ce1536e5
masonleung2016 <masonleung@163.com>
2020-07-07 16:53:20 +0800

    Create GlobalExceptionHandler.java

---
+++ khr-web/src/main/java/com/ufostudio/khr/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.ufostudio.khr.exception;
+
+import com.ufostudio.khr.model.RespBean;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 10:28
+ * @Package: com.ufostudio.khr.exception
+ */
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SQLException.class)
+    public RespBean sqlException(SQLException e) {
+        if (e instanceof SQLIntegrityConstraintViolationException) {
+            return RespBean.error("该数据有关联数据，操作失败!");
+        }
+        return RespBean.error("数据库异常，操作失败!");
+    }
+}
