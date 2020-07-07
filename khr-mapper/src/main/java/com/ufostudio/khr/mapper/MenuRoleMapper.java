01e25d993fbf14d81442c444bd638c67edff8011
masonleung2016 <masonleung@163.com>
2020-07-07 16:19:46 +0800

    Create MenuRoleMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/MenuRoleMapper.java
@@ -0,0 +1,28 @@
+package com.ufostudio.khr.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import com.ufostudio.khr.model.MenuRole;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:29
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface MenuRoleMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(MenuRole record);
+
+    int insertSelective(MenuRole record);
+
+    MenuRole selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(MenuRole record);
+
+    int updateByPrimaryKey(MenuRole record);
+
+    void deleteByRid(Integer rid);
+
+    Integer insertRecord(@Param("rid") Integer rid, @Param("mids") Integer[] mids);
+}
