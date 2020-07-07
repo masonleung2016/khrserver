7c0266a38461cf1f8070acc4ab8ce33c90a372a6
masonleung2016 <masonleung@163.com>
2020-07-07 16:20:06 +0800

    Create MsgContentMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/MsgContentMapper.java
@@ -0,0 +1,23 @@
+package com.ufostudio.khr.mapper;
+
+import com.ufostudio.khr.model.MsgContent;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:29
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface MsgContentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(MsgContent record);
+
+    int insertSelective(MsgContent record);
+
+    MsgContent selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(MsgContent record);
+
+    int updateByPrimaryKey(MsgContent record);
+}
