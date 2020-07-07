55a1814fde363abac051c93866a7a044b4bb410c
masonleung2016 <masonleung@163.com>
2020-07-07 16:31:58 +0800

    Create MailConstants.java

---
+++ khr-model/src/main/java/com/ufostudio/khr/model/MailConstants.java
@@ -0,0 +1,25 @@
+package com.ufostudio.khr.model;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:46
+ * @Package: com.ufostudio.khr.model
+ */
+
+public class MailConstants {
+    public static final Integer DELIVERING = 0;
+    //消息投递中
+    public static final Integer SUCCESS = 1;
+    //消息投递成功
+    public static final Integer FAILURE = 2;
+    //消息投递失败
+    public static final Integer MAX_TRY_COUNT = 3;
+    //最大重试次数
+    public static final Integer MSG_TIMEOUT = 1;
+    //消息超时时间
+    public static final String MAIL_QUEUE_NAME = "ufostudio.mail.queue";
+
+    public static final String MAIL_EXCHANGE_NAME = "ufostudio.mail.exchange";
+
+    public static final String MAIL_ROUTING_KEY_NAME = "ufostudio.mail.routing.key";
+}
