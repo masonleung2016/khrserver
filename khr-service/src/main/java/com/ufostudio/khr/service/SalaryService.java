fa33420956d82f9b1315d9cba4efd792f9e2a88e
masonleung2016 <masonleung@163.com>
2020-07-07 16:38:08 +0800

    Create SalaryService.java

---
+++ khr-service/src/main/java/com/ufostudio/khr/service/SalaryService.java
@@ -0,0 +1,38 @@
+package com.ufostudio.khr.service;
+
+import com.ufostudio.khr.mapper.SalaryMapper;
+import com.ufostudio.khr.model.Salary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:57
+ * @Package: com.ufostudio.khr.service
+ */
+
+@Service
+public class SalaryService {
+    @Autowired
+    SalaryMapper salaryMapper;
+
+    public List<Salary> getAllSalaries() {
+        return salaryMapper.getAllSalaries();
+    }
+
+    public Integer addSalary(Salary salary) {
+        salary.setCreateDate(new Date());
+        return salaryMapper.insertSelective(salary);
+    }
+
+    public Integer deleteSalaryById(Integer id) {
+        return salaryMapper.deleteByPrimaryKey(id);
+    }
+
+    public Integer updateSalaryById(Salary salary) {
+        return salaryMapper.updateByPrimaryKeySelective(salary);
+    }
+}
