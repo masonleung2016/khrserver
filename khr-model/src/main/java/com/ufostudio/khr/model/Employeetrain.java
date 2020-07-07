2c05bef18f598d587e35c1fb070899eda1398d85
masonleung2016 <masonleung@163.com>
2020-07-07 16:25:18 +0800

    Create Employeetrain.java

---
+++ khr-model/src/main/java/com/ufostudio/khr/model/Employeetrain.java
@@ -0,0 +1,61 @@
+package com.ufostudio.khr.model;
+
+import java.util.Date;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:40
+ * @Package: com.ufostudio.khr.model
+ */
+
+public class Employeetrain {
+    private Integer id;
+
+    private Integer eid;
+
+    private Date traindate;
+
+    private String traincontent;
+
+    private String remark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getEid() {
+        return eid;
+    }
+
+    public void setEid(Integer eid) {
+        this.eid = eid;
+    }
+
+    public Date getTraindate() {
+        return traindate;
+    }
+
+    public void setTraindate(Date traindate) {
+        this.traindate = traindate;
+    }
+
+    public String getTraincontent() {
+        return traincontent;
+    }
+
+    public void setTraincontent(String traincontent) {
+        this.traincontent = traincontent == null ? null : traincontent.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+}
