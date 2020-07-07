72fdc288105e2864d306c1ed736f98a471c667d4
masonleung2016 <masonleung@163.com>
2020-07-07 16:20:24 +0800

    Create NationMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/NationMapper.java
@@ -0,0 +1,27 @@
+package com.ufostudio.khr.mapper;
+
+import com.ufostudio.khr.model.Nation;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:29
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface NationMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Nation record);
+
+    int insertSelective(Nation record);
+
+    Nation selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Nation record);
+
+    int updateByPrimaryKey(Nation record);
+
+    List<Nation> getAllNations();
+}
