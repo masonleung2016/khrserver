97d43bd4a20847b3d6050c07c5b7f7f05be9f261
masonleung2016 <masonleung@163.com>
2020-07-07 16:43:39 +0800

    Create WebSocketConfig.java

---
+++ khr-web/src/main/java/com/ufostudio/khr/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.ufostudio.khr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 10:16
+ * @Package: com.ufostudio.khr.config
+ */
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/ep").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue");
+    }
+}
