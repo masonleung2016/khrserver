3a1b7e197f63b31facf2be4be48693a9a12b3c36
masonleung2016 <masonleung@163.com>
2020-07-07 16:19:00 +0800

    Create MailSendLogMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/MailSendLogMapper.java
@@ -0,0 +1,23 @@
+package com.ufostudio.khr.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import com.ufostudio.khr.model.MailSendLog;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:28
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface MailSendLogMapper {
+    Integer updateMailSendLogStatus(@Param("msgId") String msgId, @Param("status") Integer status);
+
+    Integer insert(MailSendLog mailSendLog);
+
+    List<MailSendLog> getMailSendLogsByStatus();
+
+    Integer updateCount(@Param("msgId") String msgId, @Param("date") Date date);
+}
