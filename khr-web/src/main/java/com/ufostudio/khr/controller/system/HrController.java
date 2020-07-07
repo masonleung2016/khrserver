c1d43b9159ad231abdcaeb488049416a229ac29c
masonleung2016 <masonleung@163.com>
2020-07-07 16:48:08 +0800

    Create HrController.java

---
+++ khr-web/src/main/java/com/ufostudio/khr/controller/system/HrController.java
@@ -0,0 +1,58 @@
+package com.ufostudio.khr.controller.system;
+
+import com.ufostudio.khr.model.Hr;
+import com.ufostudio.khr.model.RespBean;
+import com.ufostudio.khr.model.Role;
+import com.ufostudio.khr.service.HrService;
+import com.ufostudio.khr.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 10:24
+ * @Package: com.ufostudio.khr.controller.system.basic
+ */
+
+@RestController
+@RequestMapping("/system/hr")
+public class HrController {
+    @Autowired
+    HrService hrService;
+    @Autowired
+    RoleService roleService;
+    @GetMapping("/")
+    public List<Hr> getAllHrs(String keywords) {
+        return hrService.getAllHrs(keywords);
+    }
+
+    @PutMapping("/")
+    public RespBean updateHr(@RequestBody Hr hr) {
+        if (hrService.updateHr(hr) == 1) {
+            return RespBean.ok("更新成功!");
+        }
+        return RespBean.error("更新失败!");
+    }
+    @GetMapping("/roles")
+    public List<Role> getAllRoles() {
+        return roleService.getAllRoles();
+    }
+
+    @PutMapping("/role")
+    public RespBean updateHrRole(Integer hrid, Integer[] rids) {
+        if (hrService.updateHrRole(hrid, rids)) {
+            return RespBean.ok("更新成功!");
+        }
+        return RespBean.error("更新失败!");
+    }
+
+    @DeleteMapping("/{id}")
+    public RespBean deleteHrById(@PathVariable Integer id) {
+        if (hrService.deleteHrById(id) == 1) {
+            return RespBean.ok("删除成功!");
+        }
+        return RespBean.error("删除失败!");
+    }
+}
