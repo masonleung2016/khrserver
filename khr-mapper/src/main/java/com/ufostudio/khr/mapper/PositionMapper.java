0b29be98830a9d45f8155a15cb1b675f9154a696
masonleung2016 <masonleung@163.com>
2020-07-07 16:21:23 +0800

    Create PositionMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/PositionMapper.java
@@ -0,0 +1,30 @@
+package com.ufostudio.khr.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import com.ufostudio.khr.model.Position;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:31
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface PositionMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Position record);
+
+    int insertSelective(Position record);
+
+    Position selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Position record);
+
+    int updateByPrimaryKey(Position record);
+
+    List<Position> getAllPositions();
+
+    Integer deletePositionsByIds(@Param("ids") Integer[] ids);
+}
