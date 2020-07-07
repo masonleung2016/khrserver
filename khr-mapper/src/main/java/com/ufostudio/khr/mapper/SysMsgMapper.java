64f29b3bb52fe4bc514d1ab5ad143e985724735c
masonleung2016 <masonleung@163.com>
2020-07-07 16:22:24 +0800

    Create SysMsgMapper.java

---
+++ khr-mapper/src/main/java/com/ufostudio/khr/mapper/SysMsgMapper.java
@@ -0,0 +1,23 @@
+package com.ufostudio.khr.mapper;
+
+import com.ufostudio.khr.model.SysMsg;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/7 9:31
+ * @Package: com.ufostudio.khr.mapper
+ */
+
+public interface SysMsgMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysMsg record);
+
+    int insertSelective(SysMsg record);
+
+    SysMsg selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysMsg record);
+
+    int updateByPrimaryKey(SysMsg record);
+}
